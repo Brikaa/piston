9f2bbcd4d41e6ee13f7592867d59703589757f34
Omar Brikaa <brikaaomar@gmail.com>
2022-08-20 23:13:32 +0200

    Nix: Python3 include isntalled pacakges site-packages

--- runtimes/python3.nix
+++ runtimes/python3.nix
@@ -12,7 +12,10 @@ in piston.mkRuntime {
     ];
 
     run = ''
-    PYTHONPATH=$PISTON_PACKAGES_PATH ${pkg}/bin/python3 "$@"
+    FIRST_PATH="''${PISTON_PACKAGES_PATH%%:*}"
+    PYTHON_LIB=$(ls $FIRST_PATH/lib | head -n1)
+    # include /lib/pythonx.x/site-packages after each path
+    PYTHONPATH="''${PISTON_PACKAGES_PATH//://lib/$PYTHON_LIB/site-packages:}/lib/$PYTHON_LIB/site-packages" ${pkg}/bin/python3 "$@"
     '';
 
     packages = pkg.pkgs;
